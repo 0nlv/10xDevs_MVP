f04065c57be3ad0e08b9fd8733a9e2d5c8c3902d
Jezierski <aleksy.jezierski@ing.com>
2026-07-31 09:05:35 +0200

    Switch to Cloudflare Pages mode in adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,9 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    mode: "pages",
+  }),
   env: {
     schema: {
       SUPABASE_URL: envField.string({ context: "server", access: "secret", optional: true }),
